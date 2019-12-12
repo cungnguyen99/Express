e102d0e4f401fc66e9c97f73778f8bdfaa808c68
Cung Nguyen <cungnv2912@gmail.com>
2019-12-12 02:41:18 -0800

    Lesson 2 learn about Template engines

--- Ex1/index.js
+++ Ex1/index.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
+/**
+ * Khi muốn lấy về một dòng html thì ta viết như ở dưới nhưng trên thực tế nó sẽ trả về cả 1 trang html nên không
+ * thể viết như thế
+
+ * app.get('/', (req, res) => res.send('<h1>Hello World! My name is Cung</h1><a href="/users"> Get users</a>'))
+
+ * app.get('/users', (req, res) => res.send('List users: '))
+ */
+//Khi muốn load cả 1 trang ta phải làm như này
+app.set('view engine', 'pug');
+
+app.set('views', './views');
+
+//Truyền vào 2 biến 1 là tên file pug 2 là 1 obj, khi muốn lấy gtri của key, bên file pug ta sẽ ghi #{name}
+app.get('/', (req, res) => res.render('index.pug',{name: 'Cung'}))
+
+app.get('/users', (req, res) => res.render('users/index.pug',{
+    userList:[
+    {id: 1, name: 'Selena Gomez'},
+    {id: 2, name: 'Anne Kedrick'}
+]}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

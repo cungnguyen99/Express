133da51cb990308b5e5ab85bbce4985b6d72587b
Cung Nguyen <cungnv2912@gmail.com>
2019-12-17 01:02:38 -0800

    Lesson 8 learn about Controller: done

--- Ex8/routes/users.route.js
+++ Ex8/routes/users.route.js
@@ -1,54 +1,17 @@
 var express = require('express')
+
 var router = express.Router(); 
-const shortid=require('shortid')
-var db=require('../db.js')
 
-router.get('/', (req, res) => res.render('users/index.pug',{
+var controller=require('../controllers/user.controller');
 
-    userList:db.get('users').value() 
+router.get('/', controller.index)
 
-}))
+router.get('/search', controller.search)
 
-router.get('/search',function(req, res){ 
+router.get('/create', controller.create)
 
-    var val=req.query.nameQuery;
+router.get('/:userId', controller.get)
 
-    var matchedArr=db.get('users').value().filter(function(user){
-
-        return user.name.toLowerCase().indexOf(val.toLowerCase()) !== -1;
-
-    })
-
-    res.render('users/index.pug',{
-        userList:matchedArr
-    })
-    
-})
-
-router.get('/create',function(rep,res){
-
-    res.render('users/create.pug');
-
-})
-
-router.get('/:userId', function(req, res){
-    
-    var idSearch=req.params.userId;
-
-    var user=db.get('users').find({id: idSearch}).value();
-
-    res.render('users/view',{
-        userInfo: user       
-    });
-})
-
-router.post('/create',function(req,res){
-
-    req.body.id=shortid.generate();
-
-    db.get('users').push(req.body).write();
-
-    res.redirect('/users');
-})
+router.post('/create', controller.postCreate)
 
 module.exports=router

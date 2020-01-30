3b8d26d7b49cc85f21a2d9e7bb3fc02e3decc593
Cung Nguyen <cungnv2912@gmail.com>
2020-01-29 20:18:48 -0800

    fix method postCreate of product: not done

--- Ex10/controllers/product.controller.js
+++ Ex10/controllers/product.controller.js
@@ -2,6 +2,48 @@
 //khong dung lowdb nua ma dùng mongo
 var Product = require('../models/products.model')
 
+module.exports.postCreate=function(req,res){
+    
+    req.body.url=req.file.path.split(/[\\/]/).slice(1).join('/')
+    req.body.url_1=req.file.path.split(/[\\/]/).slice(1).join('/')
+    req.body.url_2=req.file.path.split(/[\\/]/).slice(1).join('/')
+    req.body.url_3=req.file.path.split(/[\\/]/).slice(1).join('/')
+    req.body.url_4=req.file.path.split(/[\\/]/).slice(1).join('/')
+
+    var data=new Product({
+        name: req.body.name,
+        genre: req.body.genre,
+        country: req.body.country,    
+        time: req.body.time,
+        cast: req.body.cast,
+        imdb: req.body.imdb,
+        manufacture_year: req.body.year,
+        image: req.body.url,
+        image_1: req.body.url_1,
+        image_2: req.body.url_2,
+        image_3: req.body.url_3,
+        image_4:req.body.url_4,
+        description: req.body.description,
+    })
+    
+    data.save();
+
+    res.redirect('/product');
+}
+
+module.exports.create=function(rep,res){
+
+    Product.find().then(function (users) {
+
+        res.render('product/index.pug',{
+
+            fimls: users
+
+        })
+
+    })
+}
+
 module.exports.index = function (req, res) {
 
     //page bang gia tri cua bien page khi truy cap neu co con khong thi bang 1. vd product?page=3
--- Ex10/routes/product.route.js
+++ Ex10/routes/product.route.js
@@ -2,8 +2,22 @@ var express = require('express')
 
 var router = express.Router(); 
 
+var multer  = require('multer')
+
+var upload = multer({ dest: './public/uploads/' })
+
 var controller=require('../controllers/product.controller');
 
 router.get('/', controller.index)
+
+router.get('/create', controller.create)
+
+router.post('/create',
+    upload.single('url'),
+    upload.single('url_1'),
+    upload.single('url_2'),
+    upload.single('url_3'),
+    upload.single('url_4'),
+    controller.postCreate)
 
 module.exports=router

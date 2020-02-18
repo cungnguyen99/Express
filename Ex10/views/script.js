5e9eb7e11ea704a4687f6b349468cf72f62b1442
Cung Nguyen <cungnv2912@gmail.com>
2020-02-17 20:18:17 -0800

    Edit the index page of products: not done

--- Ex10/views/script.js
+++ Ex10/views/script.js
@@ -1,85 +1,85 @@
 document.addEventListener('DOMContentLoaded', function () {
     var nut = document.querySelectorAll('.chuyensilde ul li');
-    var slides=document.querySelectorAll('.cacslide ul.wrapper_li>li');
-    var tg=setInterval(function(){
+    var slides = document.querySelectorAll('.cacslide ul.wrapper_li>li');
+    var tg = setInterval(function () {
         autosilde();
-    },5000);
+    }, 5000);
     for (let index = 0; index < nut.length; index++) {
-        nut[index].addEventListener('click', function (){
+        nut[index].addEventListener('click', function () {
             for (let index = 0; index < nut.length; index++) {
                 nut[index].classList.remove('kichhoat');
             }
             this.classList.add('kichhoat');
-            var nutkichhoat=nut[index];
-            var vtrinut=0;
-            for (vtrinut = 0; nutkichhoat = nutkichhoat.previousElementSibling; vtrinut++) {}
+            var nutkichhoat = nut[index];
+            var vtrinut = 0;
+            for (vtrinut = 0; nutkichhoat = nutkichhoat.previousElementSibling; vtrinut++) { }
             for (let i = 0; i < slides.length; i++) {
                 slides[i].classList.remove('active');
                 slides[vtrinut].classList.add('active');
-                
+
             }
         })
     }
 
-    function autosilde(){
-            var vtrislide=0;
-            var slidehientai=document.querySelector('.cacslide ul.wrapper_li>li.active');
-            for (vtrislide = 0; slidehientai = slidehientai.previousElementSibling; vtrislide++) {}
-            if(vtrislide < slides.length-1){
-            for (let i = 0; i < slides.length; i++) {
-               slides[i].classList.remove('active');
-               nut[i].classList.remove('kichhoat');
-            }
-            slides[vtrislide].nextElementSibling.classList.add('active');
-            nut[vtrislide].nextElementSibling.classList.add('kichhoat');
-        }else{
-            vtrislide=0
+    function autosilde() {
+        var vtrislide = 0;
+        var slidehientai = document.querySelector('.cacslide ul.wrapper_li>li.active');
+        for (vtrislide = 0; slidehientai = slidehientai.previousElementSibling; vtrislide++) { }
+        if (vtrislide < slides.length - 1) {
             for (let i = 0; i < slides.length; i++) {
                 slides[i].classList.remove('active');
                 nut[i].classList.remove('kichhoat');
-             }
-             slides[vtrislide].classList.add('active');
-             nut[vtrislide].classList.add('kichhoat');
+            }
+            slides[vtrislide].nextElementSibling.classList.add('active');
+            nut[vtrislide].nextElementSibling.classList.add('kichhoat');
+        } else {
+            vtrislide = 0
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].classList.remove('active');
+                nut[i].classList.remove('kichhoat');
+            }
+            slides[vtrislide].classList.add('active');
+            nut[vtrislide].classList.add('kichhoat');
         }
     }
 
-    var menu=document.querySelector('nav');
-    var burger=document.querySelector('.burger')
-    var list_item=document.querySelector('.nav__list');
-    let count=1;
-    burger.addEventListener('click',function(){
-      count++;
-      if(count%2==0){
-        menu.classList.add('nav__background');
-        list_item.classList.remove('nav__list--hidden')
-        list_item.classList.add('nav__list--active ')
-      }if(count%2!=0){
-        menu.classList.remove('nav__background');
-        list_item.classList.add('nav__list--hidden')
-        list_item.classList.remove('nav__list--active ')
-      }
+    var menu = document.querySelector('nav');
+    var burger = document.querySelector('.burger')
+    var list_item = document.querySelector('.nav__list');
+    let count = 1;
+    burger.addEventListener('click', function () {
+        count++;
+        if (count % 2 == 0) {
+            menu.classList.add('nav__background');
+            list_item.classList.remove('nav__list--hidden')
+            list_item.classList.add('nav__list--active ')
+        } if (count % 2 != 0) {
+            menu.classList.remove('nav__background');
+            list_item.classList.add('nav__list--hidden')
+            list_item.classList.remove('nav__list--active ')
+        }
     })
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         },
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        navText : ["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"]
-    }) 
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"]
+    })
 
     $('.slider_center .slider').slick({
         slidesToShow: 1,
@@ -107,8 +107,29 @@ document.addEventListener('DOMContentLoaded', function () {
         infinite: true,
         cssEase: 'linear',
         vertical: true,
-        autoplaySpeed: 100
+        autoplaySpeed: 500
     });
+
+    (function () {
+        function init() {
+            var speed = 300,
+                easing = mina.backout;
+            [].slice.call(document.querySelectorAll('#grid > a')).forEach(function (el) {
+                var s = Snap(el.querySelector('svg')), path = s.select('path'),
+                    pathConfig = {
+                        from: path.attr('d'),
+                        to: el.getAttribute('data-path-hover')
+                    };
+                el.addEventListener('mouseenter', function () {
+                    path.animate({ 'path': pathConfig.to }, speed, easing);
+                });
+                el.addEventListener('mouseleave', function () {
+                    path.animate({ 'path': pathConfig.from }, speed, easing);
+                });
+            });
+        }
+        init();
+    })();
 }, false)
 
 

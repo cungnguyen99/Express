c4c8aa308673841f5f1940ff3e3ab50e9e810611
Cung Nguyen <cungnv2912@gmail.com>
2020-02-18 20:09:36 -0800

    edit the index page of product: not done

--- Ex10/views/script.js
+++ Ex10/views/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log('running...')
     var nut = document.querySelectorAll('.chuyensilde ul li');
     var slides = document.querySelectorAll('.cacslide ul.wrapper_li>li');
     var tg = setInterval(function () {
@@ -109,27 +110,6 @@ document.addEventListener('DOMContentLoaded', function () {
         vertical: true,
         autoplaySpeed: 500
     });
-
-    (function () {
-        function init() {
-            var speed = 300,
-                easing = mina.backout;
-            [].slice.call(document.querySelectorAll('#grid > a')).forEach(function (el) {
-                var s = Snap(el.querySelector('svg')), path = s.select('path'),
-                    pathConfig = {
-                        from: path.attr('d'),
-                        to: el.getAttribute('data-path-hover')
-                    };
-                el.addEventListener('mouseenter', function () {
-                    path.animate({ 'path': pathConfig.to }, speed, easing);
-                });
-                el.addEventListener('mouseleave', function () {
-                    path.animate({ 'path': pathConfig.from }, speed, easing);
-                });
-            });
-        }
-        init();
-    })();
 }, false)
 
 
